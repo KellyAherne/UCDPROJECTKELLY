a4fadff6aad298897bec98dac98e9921a00679a5
Kelly.Aherne <Kelly22a@hotmail.com>
2021-07-07 12:07:41 +0100

    Test

--- TEST.py
+++ TEST.py
@@ -3,6 +3,8 @@ import numpy as np
 
 #import dataset
 telcodata = pd.read_csv("Telco_customer_churn_project.csv")
+femalecustomers = telcodata[telcodata["gender"]== "Female"]
+femalecustomers1 = femalecustomers[["customerID", "gender", "tenure", "DeviceProtection", "Contract", "PaperlessBilling", "MonthlyCharges", "TotalCharges"]]
 
-
+print(femalecustomers1)
 
